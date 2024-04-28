48dcc647913918baeb552abba88ccd680375a841
YagaYasha <2005yonatan@gmail.com>
2024-04-28 23:19:11 +0300

    I love my mom!!!!!!!2

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -6,4 +6,6 @@ map('n','q',':close<cr>')
 map('n','<S-t>',':NvimTreeToggle<cr>')
 map('n','<C-s>',':w!<cr>')
 map('n','<C-q>',':wq!<cr>')
+map({ 'n', 'x' }, '<S-h>', '^')
+map({ 'n', 'x' }, 'L', 'g_')
 
