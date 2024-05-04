18603612954ba367e8fc236342e6a06f4cc35ca0
YagaYasha <2005yonatan@gmail.com>
2024-05-04 15:47:25 +0300

    LSP_Config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,45 @@
-return
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local on_attach = function(client, budfnr)
+        local map = function(mode, key, action)
+            vim.keymap.set(mode, key, action, { buffer = bufnr, 
+            silent = true, 
+            noremap = true 
+        })
+        end
+        map("n", "K", ":Lspsaga hover _doc<CR>")
+        map("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+        map("n", "gd", ":Lspsaga goto_definition<CR>")
+        map("n", "<leader>rf", ":Lspsaga finder<CR>")
+        map("n", "<leader>ca", ":Lspsaga rename<CR>")
+        map("n", "<leader>o", ":Lspsaga outline<CR>")
+        map("n", "<leader>rn", ":Lspsaga rename<CR>")
+end
+
+return 
 {
     "neovim/nvim-lspconfig",
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-        event = "VeryLazy"
-    },
+    event = "VeryLazy",
+    
     config = function()
-        require'lspconfig'.lua_ls.setup({ autostart = true })
+        require'lspconfig'.lua_ls.setup({
+            
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    completion = {
+                        callSnippet = "Replace",
+                    },
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            }
+        })
+        
     end
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,8 +1,17 @@
-return
+return 
 {
     "williamboman/mason.nvim",
     event = "VeryLazy",
+    dependencies = {"williamboman/mason-lspconfig.nvim"},
+
     config = function()
-        require ("mason").setup()
-    end
+        require("mason").setup(settings)
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "gopls"
+            },
+            automatic_installation = true,
+        })
+    end,
 }
